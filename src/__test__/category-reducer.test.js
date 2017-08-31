349a7887ca60eb9aba61784a87206de11a7738bd
Luis Manzo <manzosk3@gmail.com>
2017-08-30 20:47:09 -0700

    given existing state and null action, returns existing state

--- src/__test__/category-reducer.test.js
+++ src/__test__/category-reducer.test.js
@@ -5,5 +5,13 @@ describe('Category Reducer', () => {
     let testState = categoryReducer(undefined, { type: null });
     expect(testState).toEqual([]);
   });
+  test('when state exists and no action type is presented, the state should be returned', () => {
+    let state = [
+      { name: 'foo', budget: 1234 },
+      { name: 'bar', budget: 6789 }
+    ];
+    let result = categoryReducer(state, { type: null });
+    expect(result).toEqual(state);
+  })
 });
 
