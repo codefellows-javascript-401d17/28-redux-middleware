073f952ec67ee6f46cf0d6ff5a518979de4c76af
Luis Manzo <manzosk3@gmail.com>
2017-08-31 01:40:08 -0700

    adds reporter middleware and adds to store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,5 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer'
+import reporter from '../reducer/'
 
-//any time action created, store notices and something (get from part aug 29 p1)
-
-export default () => createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default () => createStore(reducer, applyMiddleware(reporter), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
